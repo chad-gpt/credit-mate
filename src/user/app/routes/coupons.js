2c71caaa2223ef75e1b5761b417afe5b1f9e82b1
Varun Koranne <korannevarun@gmail.com>
2023-04-06 07:00:28 +0530

    Added stufg

--- src/user/app/routes/coupons.js
+++ src/user/app/routes/coupons.js
@@ -4,8 +4,27 @@ const User = require("../models/user");
 const coupons = require("../models/coupons");
 router.get("/", async (req, res, next) => {
   try {
+    const coupons= await coupons.find({});
+    return res.status(200).json(coupons);
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: error.message });
   }
 });
+router.post("/", async (req, res, next) => {
+    try {
+        const { code, discount, expiryDate } = req.body;
+        const newCoupon = await new coupons({
+        code,
+        discount,
+        expiryDate,
+        });
+        newCoupon.save();
+        return res.send({ newCoupon });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server Error" });
+    }
+    }
+    
+
